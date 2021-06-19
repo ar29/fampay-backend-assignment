b201b27b5a2051e8b61e87356c2f5e42f0fe287c
Aniruddha <aniruddha.ravi@thoughtworks.com>
2021-06-19 18:52:06 +0530

    Add background/models.py

--- fampay/background/models.py
+++ fampay/background/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Video(models.Model):
+    title = models.CharField()
+    description = models.CharField()
+    publishing_datetime = models.DateTimeField()
+    thumbnail_url = models.CharField()
