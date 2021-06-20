8cd44560576ed342f6965c38c91f096991afb3e7
Aniruddha <aniruddha.ravi@thoughtworks.com>
2021-06-20 21:03:17 +0530

    Add views.py to search app in project.

--- project/search/views.py
+++ project/search/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from .models import Video
+from .serializers import VideoSerializer
+
+
+class VideoViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows videos to be viewed.
+    """
+    queryset = Video.objects.all().order_by('-published_at')
+    serializer_class = VideoSerializer
