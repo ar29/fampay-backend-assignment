6f37c2d22eec514a02b595febede9a9714ed2bdd
Aniruddha <aniruddha.ravi@thoughtworks.com>
2021-06-20 19:43:43 +0530

    Update data captured by the background task (from the youtube API) in search/models.py

--- project/search/models.py
+++ project/search/models.py
@@ -4,7 +4,8 @@ from django.db import models
 
 
 class Video(models.Model):
-    title = models.CharField()
-    description = models.CharField()
-    publishing_datetime = models.DateTimeField()
-    thumbnail_url = models.CharField()
+    id = models.CharField(max_length=255, primary_key=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    published_at = models.DateTimeField()
+    thumbnail_url = models.CharField(max_length=255)
