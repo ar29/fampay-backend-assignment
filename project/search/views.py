1483204108634a8977186b38a5b1cd76d5ce242b
Aniruddha <aniruddha.ravi@thoughtworks.com>
2021-06-20 23:41:14 +0530

    Add filterset_fields and ordering_fields along with SearchFilter and OrderingFilter filter backends in search/views.py

--- project/search/views.py
+++ project/search/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
+import django_filters.rest_framework
+from rest_framework import viewsets, generics
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 from .models import Video
 from .serializers import VideoSerializer
 
@@ -12,4 +15,8 @@ class VideoViewSet(viewsets.ModelViewSet):
     API endpoint that allows videos to be viewed.
     """
     queryset = Video.objects.all().order_by('-published_at')
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['title', 'description']
+    ordering_fields = ['published_at', 'title']
+    ordering = ['-published_at']
     serializer_class = VideoSerializer
