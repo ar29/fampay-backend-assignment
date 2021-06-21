b321033927c2b47a72830590d5e08e4106d5605f
Aniruddha <aniruddha.ravi@thoughtworks.com>
2021-06-21 17:23:56 +0530

    Add DB index to Video's published_at field in search/models.py

--- project/search/models.py
+++ project/search/models.py
@@ -7,5 +7,5 @@ class Video(models.Model):
     id = models.CharField(max_length=255, primary_key=True)
     title = models.CharField(max_length=255, db_index=True)
     description = models.CharField(max_length=1024, db_index=True)
-    published_at = models.DateTimeField()
+    published_at = models.DateTimeField(db_index=True)
     thumbnail_url = models.CharField(max_length=255)
