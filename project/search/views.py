c2b08b3b641fa5c477150cf38a8343eb999cb5ae
Aniruddha <aniruddha.ravi@thoughtworks.com>
2021-06-21 10:27:50 +0530

    Add DRF search fields to regex match title and description.

--- project/search/views.py
+++ project/search/views.py
@@ -14,9 +14,22 @@ class VideoViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows videos to be viewed.
     """
-    queryset = Video.objects.all().order_by('-published_at')
+    queryset = Video.objects.all()
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ['title', 'description']
+    search_fields = ['$title', '$description']
     ordering_fields = ['published_at', 'title']
     ordering = ['-published_at']
     serializer_class = VideoSerializer
+
+    def get_queryset(self):
+        queryset = Video.objects.all()
+        title = self.request.query_params.get('title')
+        description = self.request.query_params.get('description')
+
+        if title is not None and title != "":
+            queryset = queryset.filter(title__contains=title)
+
+        if description is not None and description != "":
+            queryset = queryset.filter(description__contains=description)
+        return queryset
